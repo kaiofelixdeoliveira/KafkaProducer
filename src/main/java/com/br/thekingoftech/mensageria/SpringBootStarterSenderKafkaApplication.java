7db27e44eb1cb0360904deb3c3e5d5bbb73bfb89
kaiofelixdeoliveira <kaio.felix.oliveira.mail@gmail.com>
2021-09-26 20:38:12 -0300

    init

---
+++ src/main/java/com/br/thekingoftech/mensageria/SpringBootStarterSenderKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.br.thekingoftech.mensageria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterSenderKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterSenderKafkaApplication.class, args);
+	}
+
+}
